8dec96a3dcd6546a356b54877b7253330f6989a9
Vaibhav Yadav <vaibhavyaduvanshi2503@gmail.com>
2025-04-20 21:37:29 +0530

    improve the ui

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")))
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'anonymous',
-    password: ''// your
+    password: ''// your password
 });
 
 let getRandomUser= ()=>{
