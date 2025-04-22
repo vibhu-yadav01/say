01c00d540fb3d561b3eb35d863d864e6fd4d7c2f
Vaibhav Yadav <vaibhavyaduvanshi2503@gmail.com>
2025-04-22 22:46:01 +0530

    bug fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 
 const connection = mysql.createConnection({
